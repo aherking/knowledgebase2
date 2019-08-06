980e900679eab49c5dcdd1884b77bfd6edf83287
a.herking <a.herking@shopware.com>
2019-08-06 11:27:47 +0200

    First Search POC, actually LIKE and not fulltext, but prepared with Index.

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Controller;
+use App\Entity\Tag;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Entry;
+class SearchController extends AbstractController
+{
+    protected $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+
+    }
+
+
+    /**
+     * @Route("/search", name="search_index")
+     */
+    public function index()
+    {
+        $criteria = ['main' => 1];
+        $tags = $this->getDoctrine()
+            ->getRepository((Tag::class))
+            ->findBy($criteria);
+
+        return $this->render('/start/index.html.twig', [ 'tags' => $tags
+        ]);
+    }
+    /**
+     * @Route("/search/{searchterm}", name="search_search")
+     */
+    public function search($searchterm)
+    {
+        $entries = $this->em->createQueryBuilder()
+            ->select(array('e','t'))
+            ->from('App\Entity\Entry', 'e')
+            ->leftJoin('e.tagID', 't')
+            ->where('e.name LIKE :searchterm')
+            ->setParameter('searchterm', $searchterm)
+            ->getQuery()
+            ->getArrayResult();
+
+        return $this->render('entry/index.html.twig', [
+            'entries' => $entries
+            ]);
+    }
+}
