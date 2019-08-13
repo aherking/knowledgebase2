6291dc8f438d54bba572802d6fd2a6c08f755300
Andre Herking <a.herking@shopware.com>
2019-08-13 11:03:01 +0000

    Added Password-Change function

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-
+use Symfony\Component\Validator\Constraints\Json;
 
 
 class SecurityController extends AbstractController
@@ -21,12 +23,7 @@ class SecurityController extends AbstractController
     public function login(Request $request)
     {
         if($request->isXmlHttpRequest()) {
-            $response = new Response();
-            $response->setContent(json_encode([
-                'success' => 'true',
-            ]));
-            $response->headers->set('Content-Type', 'application/json');
-
+            $response = new JsonResponse([ 'success' => 'true']);
             return $response;
         }
         else
@@ -37,27 +34,19 @@ class SecurityController extends AbstractController
     /**
      * @Route("/profile/changepassword", name="change_password")
      */
-    public function changePassword (Request $request, UserPasswordEncoderInterface $encoder)
+    public function changePassword (Request $request, UserPasswordEncoderInterface $encoder, EntityManagerInterface $entityManager)
     {
-        var_dump($request->getContent());
-        die();
+
         $user = $this->getUser();
         if ($user instanceof UserInterface) {
             $plainPassword = $request->request->get('password');
             $encoded = $encoder->encodePassword($user, $plainPassword);
 
-            $user->setPassword = $encoded;
-        }
-        else
-        {
-            throw new AccessDeniedHttpException();
+            $user->setPassword($encoded);
+            $entityManager->flush($user);
         }
 
-        $response = new Response();
-        $response->setContent(json_encode([
-            'success' => 'true',
-        ]));
-        $response->headers->set('Content-Type', 'application/json');
+        $response = new JsonResponse([ 'success' => 'true']);
         return $response;
     }
 
