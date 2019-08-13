e0c3adfd4a6544396ce6d9ef3eb715cd8838f301
Andre Herking <a.herking@shopware.com>
2019-08-13 07:04:13 +0000

    Various Changes and Improvements to User

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,8 +5,13 @@ use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
 
 class SecurityController extends AbstractController
 {
@@ -28,6 +33,32 @@ class SecurityController extends AbstractController
         {
             return $this->redirect("/");
         }
+    }
+    /**
+     * @Route("/profile/changepassword", name="change_password")
+     */
+    public function changePassword (Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        var_dump($request->getContent());
+        die();
+        $user = $this->getUser();
+        if ($user instanceof UserInterface) {
+            $plainPassword = $request->request->get('password');
+            $encoded = $encoder->encodePassword($user, $plainPassword);
+
+            $user->setPassword = $encoded;
+        }
+        else
+        {
+            throw new AccessDeniedHttpException();
+        }
+
+        $response = new Response();
+        $response->setContent(json_encode([
+            'success' => 'true',
+        ]));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 
 }
